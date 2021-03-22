6804e9961d442a1658d5f86e436535232e19e8c4
Shengyu Huang <kumom.huang@gmail.com>
2021-03-22 10:48:54 +0800

    Updates

---
+++ main.33b768bb401515c103c2.hot-update.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatesindarin_compiler"]("main",{
+
+/***/ "./src/components/Resizer.tsx":
+/*!************************************!*\
+  !*** ./src/components/Resizer.tsx ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Resizer)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var _Resizer_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Resizer.scss */ \"./src/components/Resizer.scss\");\n\n\nclass Resizer extends react__WEBPACK_IMPORTED_MODULE_0__.Component {\n    constructor(props) {\n        super(props);\n        this.ref = react__WEBPACK_IMPORTED_MODULE_0__.createRef();\n        this.leftRef = null;\n        this.rightRef = null;\n        this.active = false;\n        this.leftWidth = 0;\n        this.rightWidth = 0;\n        this.onMouseMove = this.onMouseMove.bind(this);\n    }\n    getWidth(el) {\n        return Number(window\n            .getComputedStyle(el)\n            .getPropertyValue(\"width\")\n            .replace(/[^0-9.]/g, \"\"));\n    }\n    onMouseMove(event) {\n        if (!this.active)\n            return;\n        event.preventDefault();\n        event.stopPropagation();\n        if (this.leftRef) {\n            this.leftWidth += event.movementX;\n        }\n        else {\n            if (this.ref.current) {\n                const el = this.ref.current.previousElementSibling;\n                if (el.classList.contains(\"panel\")) {\n                    this.leftRef = el;\n                    this.leftWidth = this.getWidth(el);\n                }\n            }\n        }\n        if (this.rightRef) {\n            this.rightWidth -= event.movementX;\n        }\n        else {\n            if (this.ref.current) {\n                const el = this.ref.current.nextElementSibling;\n                if (el.classList.contains(\"panel\")) {\n                    this.rightRef = el;\n                    this.rightWidth = this.getWidth(el);\n                }\n            }\n        }\n        if (this.leftRef && this.leftWidth > 0) {\n            this.leftRef.style.width = this.leftWidth + \"px\";\n        }\n        if (this.rightRef && this.rightWidth > 0) {\n            this.rightRef.style.width = this.rightWidth + \"px\";\n        }\n    }\n    shouldComponentUpdate() {\n        return false;\n    }\n    componentDidMount() {\n        document.addEventListener(\"mouseup\", () => {\n            this.active = false;\n        });\n        document.addEventListener(\"mouseleave\", () => {\n            this.ref.current.style.cursor = \"default\";\n        });\n        document.addEventListener(\"mousemove\", this.onMouseMove);\n    }\n    render() {\n        return (react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", { className: \"resizer\", ref: this.ref, onMouseOver: () => {\n                this.ref.current.style.cursor = \"col-resize\";\n            }, onMouseDown: () => {\n                this.active = true;\n                this.ref.current.style.cursor = \"col-resize\";\n            } }));\n    }\n}\n\n\n//# sourceURL=webpack://sindarin.compiler/./src/components/Resizer.tsx?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("ad6efb9ab2a89353409c")
+/******/ })();
+/******/ 
+/******/ }
+);
---
+++ main.cfd6888571c28f632cae.hot-update.js
@@ -0,0 +1,54 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatesindarin_compiler"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/components/Resizer.scss":
+/*!******************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/components/Resizer.scss ***!
+  \******************************************************************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);\n// Imports\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \".resizer {\\n  background: rgba(180, 180, 180, 0.8);\\n  opacity: 0.5;\\n  z-index: 1;\\n  box-sizing: border-box;\\n  background-clip: padding-box;\\n  width: 11px;\\n  margin: 0 -5px;\\n  border-left: 5px solid rgba(255, 255, 255, 0);\\n  border-right: 5px solid rgba(255, 255, 255, 0);\\n}\\n\\n.resizer:hover {\\n  border-left: 5px solid rgba(204, 204, 204, 0.3);\\n  border-right: 5px solid rgba(204, 204, 204, 0.3);\\n  transition: all 1s ease;\\n}\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://sindarin.compiler/./src/components/Resizer.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js");
+
+/***/ }),
+
+/***/ "./src/components/Resizer.scss":
+/*!*************************************!*\
+  !*** ./src/components/Resizer.scss ***!
+  \*************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ \"./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\");\n/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../../node_modules/css-loader/dist/cjs.js!../../node_modules/sass-loader/dist/cjs.js!./Resizer.scss */ \"./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/components/Resizer.scss\");\n\n            \n\nvar options = {};\n\noptions.insert = \"head\";\noptions.singleton = false;\n\nvar update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__.default, options);\n\n\nif (true) {\n  if (!_node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__.default.locals || module.hot.invalidate) {\n    var isEqualLocals = function isEqualLocals(a, b, isNamedExport) {\n  if (!a && b || a && !b) {\n    return false;\n  }\n\n  var p;\n\n  for (p in a) {\n    if (isNamedExport && p === 'default') {\n      // eslint-disable-next-line no-continue\n      continue;\n    }\n\n    if (a[p] !== b[p]) {\n      return false;\n    }\n  }\n\n  for (p in b) {\n    if (isNamedExport && p === 'default') {\n      // eslint-disable-next-line no-continue\n      continue;\n    }\n\n    if (!a[p]) {\n      return false;\n    }\n  }\n\n  return true;\n};\n    var oldLocals = _node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__.default.locals;\n\n    module.hot.accept(\n      /*! !!../../node_modules/css-loader/dist/cjs.js!../../node_modules/sass-loader/dist/cjs.js!./Resizer.scss */ \"./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/components/Resizer.scss\",\n      __WEBPACK_OUTDATED_DEPENDENCIES__ => { /* harmony import */ _node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../../node_modules/css-loader/dist/cjs.js!../../node_modules/sass-loader/dist/cjs.js!./Resizer.scss */ \"./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/components/Resizer.scss\");\n(function () {\n        if (!isEqualLocals(oldLocals, _node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__.default.locals, undefined)) {\n                module.hot.invalidate();\n\n                return;\n              }\n\n              oldLocals = _node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__.default.locals;\n\n              update(_node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__.default);\n      })(__WEBPACK_OUTDATED_DEPENDENCIES__); }\n    )\n  }\n\n  module.hot.dispose(function() {\n    update();\n  });\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_Resizer_scss__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});\n\n//# sourceURL=webpack://sindarin.compiler/./src/components/Resizer.scss?");
+
+/***/ }),
+
+/***/ "./src/components/Resizer.tsx":
+/*!************************************!*\
+  !*** ./src/components/Resizer.tsx ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Resizer)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var _Resizer_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Resizer.scss */ \"./src/components/Resizer.scss\");\n\n\nclass Resizer extends react__WEBPACK_IMPORTED_MODULE_0__.Component {\n    constructor(props) {\n        super(props);\n        this.ref = react__WEBPACK_IMPORTED_MODULE_0__.createRef();\n        this.leftRef = null;\n        this.rightRef = null;\n        this.active = false;\n        this.leftWidth = -1;\n        this.rightWidth = -1;\n        this.onMouseMove = this.onMouseMove.bind(this);\n    }\n    getWidth(el) {\n        return Number(window\n            .getComputedStyle(el)\n            .getPropertyValue(\"width\")\n            .replace(/[^0-9.]/g, \"\"));\n    }\n    onMouseMove(event) {\n        if (!this.active)\n            return;\n        event.preventDefault();\n        event.stopPropagation();\n        if (this.leftRef) {\n            this.leftWidth += event.movementX;\n        }\n        else {\n            if (this.ref.current) {\n                const el = this.ref.current.previousElementSibling;\n                if (el.classList.contains(\"panel\")) {\n                    this.leftRef = el;\n                    this.leftWidth = this.getWidth(el);\n                }\n            }\n        }\n        if (this.rightRef) {\n            this.rightWidth -= event.movementX;\n        }\n        else {\n            if (this.ref.current) {\n                const el = this.ref.current.nextElementSibling;\n                if (el.classList.contains(\"panel\")) {\n                    this.rightRef = el;\n                    this.rightWidth = this.getWidth(el);\n                }\n            }\n        }\n        if (this.leftRef && this.leftWidth > 0) {\n            this.leftRef.style.width = this.leftWidth + \"px\";\n        }\n        if (this.rightRef && this.rightWidth > 0) {\n            this.rightRef.style.width = this.rightWidth + \"px\";\n        }\n    }\n    shouldComponentUpdate() {\n        return false;\n    }\n    componentDidMount() {\n        document.addEventListener(\"mouseup\", () => {\n            this.active = false;\n        });\n        document.addEventListener(\"mouseleave\", () => {\n            this.ref.current.style.cursor = \"default\";\n        });\n        document.addEventListener(\"mousemove\", this.onMouseMove);\n    }\n    render() {\n        return (react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", { className: \"resizer\", ref: this.ref, onMouseOver: () => {\n                this.ref.current.style.cursor = \"col-resize\";\n            }, onMouseDown: () => {\n                this.active = true;\n                this.ref.current.style.cursor = \"col-resize\";\n            } }));\n    }\n}\n\n\n//# sourceURL=webpack://sindarin.compiler/./src/components/Resizer.tsx?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("33b768bb401515c103c2")
+/******/ })();
+/******/ 
+/******/ }
+);
